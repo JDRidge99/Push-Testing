6e91882324a436d99a153568bf3e3d0983812106
JDRidge99 <87364033+JDRidge99@users.noreply.github.com>
2021-09-08 11:00:08 +0100

    Add files via upload
    Added some random files

---
+++ MyCalculator/Operation.java
@@ -0,0 +1,18 @@
+public class Operation
+{
+    public static void main(String[] args)
+    {
+        double num1 = 69;
+        double num2 = 0;
+        char operator = '+';
+
+
+        System.out.println(num1);
+        System.out.println(operator);
+        System.out.println(num2);
+
+
+        // Calculator myObj = new Calculator();
+        // System.out.println(myObj.num1);
+    }
+}
